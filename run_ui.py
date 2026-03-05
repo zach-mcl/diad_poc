cf73876a80ade84ddf6e86892a2e0f7c84fbbbea
Sam Ball <sbbncd@umsystem.edu>
2026-03-04 18:27:04 -0600

    Basic DND Recognition
    Basic drag and drop UI recognition. FILES DO NOT UPLOAD WHEN DRAGGED/DROPPED.

--- run_ui.py
+++ run_ui.py
@@ -2,9 +2,11 @@ import tkinter as tk
 from UI.app import TkApp
 
 def main():
-    root = tk.Tk()
-    TkApp(root)
-    root.mainloop()
+    #root = tk.Tk()
+    #TkApp(root)
+    #root.mainloop()
+    app = TkApp()
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
